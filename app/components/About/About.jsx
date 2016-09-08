0dab9912931a9253b8747fe7c518e05212428e08
Alberto Lopez <albertolopez19@gmail.com>
2016-09-08 15:54:24 -0400

    Update about component

--- app/components/About/About.jsx
+++ app/components/About/About.jsx
@@ -1,5 +1,23 @@
+/* About section react component */
 import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
+import Paper from 'material-ui/Paper';
+
+const styles = {
+    container: {
+        paddingTop: '40%',
+    },
+    paper: {
+        display: 'inline-block',
+        // height: '300px',
+        padding: '20px',
+        // paddingTop: '30%',
+    },
+    text: {
+        // display: 'flex',
+        // alignItems: 'center',
+    }
+};
 
 const About = () => {
     return (
@@ -7,10 +25,14 @@ const About = () => {
             <Row>
                 <Col xs={12}>
                     <Row center="xs">
-                        <div>
-                            <p>Create, vote on and share polls made by you and your friends!
-                            Select a poll or sign in below to get started.</p>
-                        </div>
+                        <Col xs={6}>
+                            <div style={styles.container}>
+                                <Paper zDepth={4} style={styles.paper}>
+                                    <span style={styles.text}><p>Create, vote on and share polls made by you and your friends!
+                                    </p><br /><p>Select a poll or sign in below to get started.</p></span>
+                                </Paper>
+                            </div>
+                        </Col>
                     </Row>
                 </Col>
             </Row>
