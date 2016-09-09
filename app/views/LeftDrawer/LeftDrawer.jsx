23cc98f6a2600fc4aa21a15c1b8e5926962fd964
Alberto Lopez <albertolopez19@gmail.com>
2016-09-09 10:26:18 -0400

    Add left drawer component

---
+++ app/views/LeftDrawer/LeftDrawer.jsx
@@ -0,0 +1,38 @@
+/* Left drawer navigation component */
+import React from 'react';
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
+
+export default class LeftDrawer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+        };
+    }
+    handleToggle = () => {
+        this.setState({
+            open: !this.state.open,
+        });
+    }
+    render() {
+        return (
+            <div><IconButton
+                    onTouchTap={this.handleToggle}
+                    targetOrigin={ {horizontal: 'right', vertical: 'top'} }
+                    anchorOrigin={ {horizontal: 'right', vertical: 'top'} }>
+                    <NavigationMenu />
+                </IconButton>
+                <Drawer
+                    open={this.state.open}
+                    docked={false}
+                    onRequestChange={this.handleToggle.bind(this)}>
+                    <MenuItem>Menu Item</MenuItem>
+                    <MenuItem>Menu Item</MenuItem>
+                </Drawer>
+            </div>
+        );
+    }
+}
--- app/views/Navbar/Navbar.jsx
+++ app/views/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
 import NavbarTabs from './NavbarTabs.jsx';
+import LeftDrawer from '../LeftDrawer/LeftDrawer.jsx';
 
 const NavBar = () => (
     <AppBar
         title="Polling"
+        iconElementLeft={<LeftDrawer />}
         iconElementRight={<NavbarTabs />}
     />
 );
